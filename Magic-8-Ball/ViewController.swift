c5a8702be394c6246042e553f3ea1b0a9d7b597c
Josileudo <josileudorodrigues75@gmail.com>
2022-11-05 16:21:11 -0300

    :tada: Start project

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Josileudo Rodrigues de Freitas on 05/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
