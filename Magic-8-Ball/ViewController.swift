0d9c02cc4bb1f7a6db5b29ed0906ba4592602abb
Josileudo <josileudorodrigues75@gmail.com>
2022-11-14 18:59:58 -0300

    :lipstick: Feat: Create random images with button click

--- Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageSetView: UIImageView!
+    
+    var magicImageBalls = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonAskPressed(_ sender: UIButton) {
+        imageSetView.image = magicImageBalls[Int.random(in: 0...4)]
+    }
 }
-
